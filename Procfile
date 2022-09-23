53b2c9c9ebd3b7a38252f8998bd7e62b5773437f
akshaya <akshayapramod73@gmail.com>
2022-09-23 20:55:45 +0530

    added school deails

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn SCHOOLERP.wsgi --log-file -
--- SCHOOLERP/settings.py
+++ SCHOOLERP/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n6^bg+=brp^daeu=^4a6$(-65*it6x=7u+wx#-rpmq9xmm@05j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
